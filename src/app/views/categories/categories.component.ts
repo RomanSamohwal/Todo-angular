36feac80824e9395654e26ff51070416dc79fa96
Roman <rsamohwal@gmail.com>
2021-10-19 19:03:59 +0300

    добавили отображении  задачи по категории в конслоль

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -19,4 +19,9 @@ export class CategoriesComponent implements OnInit {
     this.categories = this.dataHandler.getCategories()
   }
 
+
+  showTasksByCategory(category: Category): void {
+    this.dataHandler.getTasksByCategory(category)
+  }
+
 }
